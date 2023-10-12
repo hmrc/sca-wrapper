a6d964cd5ad5514e85510983dd3bd6b5cef309a7
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2023-10-12 17:32:12 +0100

    SO-9 | Moved css and js

--- src/main/scala/uk/gov/hmrc/sca/controllers/Assets.scala
+++ src/main/scala/uk/gov/hmrc/sca/controllers/Assets.scala
@@ -17,8 +17,8 @@
 package uk.gov.hmrc.sca.controllers
 
 import controllers.{AssetsBuilder, AssetsMetadata}
-import javax.inject.{Inject, Singleton}
 import play.api.http.HttpErrorHandler
+import javax.inject.{Inject, Singleton}
 
 /*
  * Without this we get
