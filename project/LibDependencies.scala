f101fb4fbae5ff71679482e872f2e1fadb9a2535
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2023-12-27 16:33:45 +0000

    SO-300 | Tidying

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -1,34 +1,28 @@
 import sbt._
 
-
-
 object LibDependencies {
-
-  val play28Version = "2.8.20"
-  val play29Version = "2.9.4"
-  val play30Version = "3.1.0"
 
   val play28 = Seq(
     "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-28"       % "8.1.0",
-    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-28"      % "8.1.0",
+    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-28"       % "8.1.0",
     "uk.gov.hmrc"                 %% "play-language"                    % s"6.2.0-play-28",
     "uk.gov.hmrc"                 %% "play-partials"                    % s"8.4.0-play-28",
     "uk.gov.hmrc"                 %% "domain"                           % s"8.3.0-play-28"
   )
 
   val play28Test = Seq(
-    "uk.gov.hmrc"                   %% "bootstrap-test-play-28"        % "8.1.0" % Test,
+    "uk.gov.hmrc"                   %% "bootstrap-test-play-28"         % "8.1.0" % Test,
     "org.scalatestplus"             %% "mockito-4-6"                    % "3.2.15.0" % Test,
     "com.github.tomakehurst"         %  "wiremock-jre8"                 % "2.35.0" % Test,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"           % "2.14.2" % Test,
-    "com.vladsch.flexmark"            %  "flexmark-all"                   % "0.64.8" % Test
+    "com.vladsch.flexmark"            %  "flexmark-all"                 % "0.64.8" % Test
   )
 
   val play29 = Seq(
     "com.typesafe.play"           %% s"play-json"                       % "2.9.4",
     "uk.gov.hmrc"                 %% s"domain-play-29"                  % "9.0.0",
     "uk.gov.hmrc"                 %% s"play-frontend-hmrc-play-29"      % "8.1.0",
-    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-29"      % "8.1.0",
+    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-29"       % "8.1.0",
     "uk.gov.hmrc"                 %% s"play-language-play-29"           % "7.0.0",
     "uk.gov.hmrc"                 %% s"play-partials-play-29"           % "9.1.0"
   )
@@ -41,9 +35,9 @@ object LibDependencies {
 
   val play30 = Seq(
     "org.playframework"           %% "play-json"                        % "3.0.1",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"      % "8.1.0",
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"       % "8.1.0",
     "uk.gov.hmrc"                 %% s"domain-play-30"                  % "9.0.0",
-    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"      % "8.1.0",
+    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"       % "8.1.0",
     "uk.gov.hmrc"                 %% s"play-language-play-30"           % "7.0.0",
     "uk.gov.hmrc"                 %% s"play-partials-play-30"           % "9.1.0"
  )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -16,6 +16,13 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.15.0")
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.3")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.5")
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.4.0")
+//
+//sys.env.get("PLAY_VERSION") match {
+//  case Some("2.8") =>
+//    addSbtPlugin("org.scalastyle" % "scalastyle-sbt-plugin" % "1.0.0")
+//  case _           =>
+//    addSbtPlugin("org.scalastyle" % "scalastyle-sbt-plugin" % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
+//}
 
 sys.env.get("PLAY_VERSION") match {
   case Some("2.8") =>
