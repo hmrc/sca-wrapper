fb7931858ba93c1a26be2e224dabece75c85619a
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-03-14 16:17:25 +0000

    added scoverage threshold for branches

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -5,6 +5,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageMinimumStmtTotal := 85,
+    ScoverageKeys.coverageMinimumBranchTotal := 70,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true,
     ScoverageKeys.coverageExcludedPackages:= "<empty>;uk.gov.hmrc.sca.viewmodels.*;.*Routes.*;.*Module.*;"
