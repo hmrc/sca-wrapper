6ef0d67a0d92b36ea9f442b6632c85afed46be5a
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-03-14 16:13:08 +0000

    increase coverage threshold

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -4,7 +4,7 @@ import scoverage.ScoverageKeys
 object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
-    ScoverageKeys.coverageMinimumStmtTotal := 79,
+    ScoverageKeys.coverageMinimumStmtTotal := 85,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true,
     ScoverageKeys.coverageExcludedPackages:= "<empty>;uk.gov.hmrc.sca.viewmodels.*;.*Routes.*;.*Module.*;"
