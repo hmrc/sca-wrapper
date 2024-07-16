5685326b9fa7fed9a418c804e6aa3f0788201f50
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-07-16 13:51:55 +0100

    missed changes

--- publishLocal.sh
+++ publishLocal.sh
@@ -5,11 +5,5 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-env PLAY_VERSION=2.8 sbt +publishLocal
 env PLAY_VERSION=2.9 sbt +publishLocal
 env PLAY_VERSION=3.0 sbt +publishLocal
-
-# NOTE:
-# we have separate sbt build config for each play version and not all versions are
-# cross compatible with the same scala versions. For example, 2.8 is our last to
-# have support for scala 2.12
