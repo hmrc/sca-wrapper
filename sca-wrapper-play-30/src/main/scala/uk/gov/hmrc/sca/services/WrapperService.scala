4697c18f97f2b17ed84d71925e0c13dc397afff5
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-07-22 16:51:09 +0100

    DDCNL: update to scala 3

--- sca-wrapper-play-30/src/main/scala/uk/gov/hmrc/sca/services/WrapperService.scala
+++ sca-wrapper-play-30/src/main/scala/uk/gov/hmrc/sca/services/WrapperService.scala
@@ -76,6 +76,7 @@ class WrapperService @Inject() (
       case (None, false)    => throw new RuntimeException("The PTA menu cannot be shown without a signout url")
       case (None, true)     => false
     }
+    println("PPPPO " + bannerConfig)
     scaLayout(
       menu = ptaMenuBar(sortMenuItemConfig(signoutUrl)),
       serviceNameKey = serviceNameKey,
