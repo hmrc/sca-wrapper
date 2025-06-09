ef1dd330f560757fe4c9ad2963d90a3d3f9c1b35
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-06-09 09:39:21 +0100

    DDCNL: update play-frontend-hmrc to 12.5.0

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibDependencies {
   val bootstrapVersion = "9.12.0"
-  private val playHmrcFrontendVersion = "12.3.0"
+  private val playHmrcFrontendVersion = "12.5.0"
   private val webchatVersion = "1.7.0"
 
 
