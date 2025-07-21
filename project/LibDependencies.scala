8c955917e106de856c4ff939580294d860679f29
SandeepUmredkar <1963092+SandeepUmredkar@users.noreply.github.com>
2025-07-21 16:04:04 +0100

    DDCNL-10793

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -9,7 +9,7 @@ object LibDependencies {
   val play30: Seq[ModuleID] = Seq(
     "org.playframework"           %% "play-json"                        % "3.0.5",
     "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"       % playHmrcFrontendVersion,
-    "uk.gov.hmrc"                 %% "domain-play-30"                  % "12.1.0",
+    "uk.gov.hmrc"                 %% "domain-play-30"                  % "11.0.0",
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc"                 %% "play-partials-play-30"           % "10.1.0",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % webchatVersion
