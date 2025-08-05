78b0165132b1be0ebf92fd6178f2c1e2101dbef2
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-08-05 10:09:03 +0100

    Update bootstrap

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object LibDependencies {
-  val bootstrapVersion = "9.17.0"
+  val bootstrapVersion = "9.18.0"
   private val playHmrcFrontendVersion = "12.7.0"
   private val webchatVersion = "1.8.0"
 
