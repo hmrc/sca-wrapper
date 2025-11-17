46b32de678f802e69efd7228f6042818943f9b1b
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-11-17 12:06:13 +0000

    update play-partial

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -11,7 +11,7 @@ object LibDependencies {
     "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"       % playHmrcFrontendVersion,
     "uk.gov.hmrc"                 %% "domain-play-30"                  % "11.0.0",
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "play-partials-play-30"           % "10.1.0",
+    "uk.gov.hmrc"                 %% "play-partials-play-30"           % "10.2.0",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % webchatVersion
  )
 
