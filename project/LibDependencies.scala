ef33273d49bfed2a8e08f4e63d4b5062c15b244c
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-11-20 09:23:29 +0000

    updates (#124)
    * updates

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 object LibDependencies {
-  val bootstrapVersion = "10.1.0"
-  private val playHmrcFrontendVersion = "12.8.0"
+  val bootstrapVersion = "10.4.0"
+  private val playHmrcFrontendVersion = "12.20.0"
   private val webchatVersion = "1.8.0"
 
 
   val play30: Seq[ModuleID] = Seq(
-    "org.playframework"           %% "play-json"                        % "3.0.5",
+    "org.playframework"           %% "play-json"                        % "3.0.6",
     "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"       % playHmrcFrontendVersion,
     "uk.gov.hmrc"                 %% "domain-play-30"                  % "11.0.0",
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "play-partials-play-30"           % "10.1.0",
+    "uk.gov.hmrc"                 %% "play-partials-play-30"           % "10.2.0",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % webchatVersion
  )
 
