b1cc541b726aaf1b84f9a5c048cf7ebb219f3f4c
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-01-19 13:42:31 +0000

    Change domain-play-30 version to 11.0.0
    Updated domain-play-30 dependency version to 11.0.0 to maintain compatibility with Scala 1.13.

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -9,7 +9,7 @@ object LibDependencies {
   val play30: Seq[ModuleID] = Seq(
     "org.playframework"           %% "play-json"                        % "3.0.6",
     "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"       % playHmrcFrontendVersion,
-    "uk.gov.hmrc"                 %% "domain-play-30"                  % "13.0.0",
+    "uk.gov.hmrc"                 %% "domain-play-30"                  % "11.0.0", // pinned to 11.0.0 due to support of scala 1.13 dropped
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc"                 %% "play-partials-play-30"           % "10.2.0",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % webchatVersion
